e26ca55e93debfba3694f5a06aa4a374cf13a490
phamtuan0611 <phamtuan06112003@gmail.com>
2024-11-20 11:31:34 +0700

    Make Color Swap

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,25 @@ public class Cell : MonoBehaviour
     [HideInInspector] public Color Color;
     [HideInInspector] public Vector2Int Position;
 
+    public bool IsStartTweenPlaying => startAnimation.IsActive();
+    public bool IsStartMovePlaying => startMoveAnimation.IsActive();
+    public bool hasSelectedMoveFinished => !selectedMoveAnimationTime.IsActive();
+    public bool hasMoveFinished => !moveAnimation.IsActive(); 
+
     [SerializeField] private SpriteRenderer _bgSprite;
+    [SerializeField] private float _startScaleDelay = 0.04f;
+    [SerializeField] private float _startScaleTime = 0.2f;
+    [SerializeField] private float _startMoveAnimationTime = 0.32f;
+    [SerializeField] private float _selectedmoveAnimationTime = 0.16f;
+    [SerializeField] private float _moveAnimationTime = 0.32f;
+
+    private Tween startAnimation;
+    private Tween startMoveAnimation;
+    private Tween selectedMoveAnimationTime;
+    private Tween moveAnimation;
+
+    private const int FRONT = 1;
+    private const int BACK = 0;
 
     public void Init(Color color, int x, int y)
     {
@@ -16,5 +35,20 @@ public class Cell : MonoBehaviour
         _bgSprite.color = Color;
         Position = new Vector2Int(x, y);
         transform.localPosition = new Vector3(x, y, 0);
+
+        //Hien co su dung Delay
+        transform.localScale = Vector3.zero;
+        float delay = (x + y) * _startScaleDelay;
+        startAnimation = transform.DOScale(1f, _startScaleTime);
+        startAnimation.SetEase(Ease.OutExpo); //Hieu ung cham dan khi sap ket thuc
+        startAnimation.SetDelay(0.5f + delay);
+        startAnimation.Play();
+    }
+
+    public void AniamteStartPositon()
+    {
+        startMoveAnimation = transform.DOLocalMove(new Vector3(Position.x, Position.y, 0), _startMoveAnimationTime);
+        startMoveAnimation.SetEase(Ease.InSine); //Hieu ung bat dau cham roi nhanh dan (nguoc lai voi OutExpo
+        startMoveAnimation.Play();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -52,6 +52,7 @@ public class GameManager : MonoBehaviour
         Rows = _currentlevelData.Row;
         Cols = _currentlevelData.Col;
 
+        //Hien thi Text
         levelNum = PlayerPrefs.GetInt("Level", 1); //Mac dinh la 1 neu khong co du lieu
         moveNum = 0;
         bestNum = PlayerPrefs.GetInt("Best" + levelNum.ToString(), 0); //Lay so diem tot nhat tu LEVEL HIEN TAI, mac dinh la 0 neu khong co du lieu
@@ -61,6 +62,7 @@ public class GameManager : MonoBehaviour
 
         DOTween.defaultAutoPlay = AutoPlay.None; //Cac hieu ung DOTWeen khong tu dong chay, phai duoc chi dinh moi chay
 
+        //Button Play
         playStartTween = _playButtonTransform.DOScale(_playButtonTransform.localScale * 1.1f, 1f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
         //DOScale làm cho đối tượng thay đổi kích thước
         //SetEase(Ease.Linear) chỉ định kiểu chuyển động tuyến tính
@@ -68,6 +70,7 @@ public class GameManager : MonoBehaviour
 
         playStartTween.Play();
 
+        //Sinh Cells
         SpawnCells();
     }
 
@@ -85,14 +88,70 @@ public class GameManager : MonoBehaviour
                 float xLerp = (float)y / (Cols - 1);
                 float yLerp = (float)x / (Rows - 1);
 
+                //Lefp la ham chon mau
+                //Neu yLerp = 0 thi mau se la _currentlevelData.BottomLeftColor
+                //Neu yLerp = 1 thi mau se la _currentlevelData.TopLeftColor
+                //Neu 0 < yLerp < 1 thi mau se la mot mau pha tron giua 2 mau kia theo ti le yLerp
                 Color leftColor = Color.Lerp(_currentlevelData.BottomLeftColor, _currentlevelData.TopLeftColor, yLerp);
                 Color rightColor = Color.Lerp(_currentlevelData.BottomRightColor, _currentlevelData.TopRightColor, yLerp);
-                Color currentColor = Color.Lerp(leftColor, rightColor, xLerp);
+                Color currentColor = Color.Lerp(leftColor, rightColor, xLerp); //Kieu se lay mau cua doc o 2 ben ngoai truoc, xong moi toi 6 hang doc o giua
 
                 correctColors[x, y] = currentColor;
                 cells[x, y] = Instantiate(_cellPrefab, _gridParent);
-                cells[x, y].Init(currentColor, y, x);
+                cells[x, y].Init(currentColor, y, x); //De y, x thay vi x, y nhu Ham boi vi neu de x, y thi mau se bi xoay ngang
             }
         }
+    }
+
+    public void ClickedPlayButton()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Rows; j++)
+            {
+                if (cells[i, j].IsStartTweenPlaying)
+                {
+                    return;
+                }
+            }
+        }
+
+        //playStartTween.Kill();
+        //playStartTween = null;
+
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+            {
+                if (_currentlevelData.LockerCells.Contains(new Vector2Int(i, j))){
+                    continue;
+                }
+
+                int swapX, swapY;
+                do
+                {
+                    swapX = Random.Range(0, Rows);
+                    swapY = Random.Range(0, Cols);
+                } while (_currentlevelData.LockerCells.Contains(new Vector2Int(swapX, swapY)));
+
+                Cell temp = cells[i, j];
+                cells[i, j] = cells[swapX, swapY];
+                Vector2Int swappedPosition = cells[swapX, swapY].Position;
+                cells[i, j].Position = temp.Position;
+                cells[swapX, swapY] = temp;
+                temp.Position = swappedPosition;
+            }
+        }
+
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+            {
+                cells[i, j].AniamteStartPositon();
+            }
+        }
+
+        hasGameStarted = true;
+        _playButtonTransform.gameObject.SetActive(false);
     }
 }
